922b28f48b0a2b89976921c44f6d17e96e9d8779
Khatuna <you@example.com>
2023-08-19 00:36:21 +0200

    mongo credencial

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('connected to database')
-    } catch(err) {
-        console.log(err);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('connected to database');
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default connectDB;
