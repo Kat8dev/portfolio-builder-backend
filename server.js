eca3c481da479b169fb0f174442a3adb9858f61b
Khatuna <you@example.com>
2023-08-27 21:08:30 +0200

    fix bug

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
-import productRoutes from './routes/product.js';
 import projectRoutes from './routes/project.js';
-import orderRoutes from './routes/order.js';
-import cartRoutes from './routes/cart.js';
 import stripeRoute from './routes/stripe.js';
 import connectDB from './config/dbConnect.js';
 import cors from 'cors';
@@ -18,10 +15,7 @@ app.use(cors());
 
 app.use('/', authRoutes);
 app.use('/', userRoutes);
-app.use('/', productRoutes);
 app.use('/', projectRoutes);
-app.use('/', orderRoutes);
-app.use('/', cartRoutes);
 app.use('/', stripeRoute);
 
 app.listen(8080, () => {
